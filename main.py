6d5329bb316b2a09afad48209124f2008f808906
argen7um <argen7um.music@yandex.ru>
2023-08-06 18:50:20 +0700

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from dataprocess.data_preprocesser import DataProcesser 
+from model.model import BinaryTextClassifier
+from model.trainer import Trainer
+from torch.nn import BCEWithLogitsLoss
+from torch.optim import Adam
+import torch
+from pprint import pprint
 
 
-dper = DataProcesser(10_000)
+LEARNING_RATE = 1e-3
+N_EPOCH = 20
+
+
+model_params = {
+    "embedding_matrix" : 'ff', 
+    "hidden_dim"       : 50,
+    "output_labels"    : 1
+}
+
+device =  torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = BinaryTextClassifier(**model_params)
+criterion = BCEWithLogitsLoss()
+optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
